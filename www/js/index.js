b653e44a2feecea8ab6c0dc99f691d665d7c37dc
QuadranteServizi <ced@quadranteservizi.it>
2014-03-24 15:03:43 +0100

    083

--- www/js/index.js
+++ www/js/index.js
@@ -9,13 +9,14 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener('online', this.onLine, false);
+        document.addEventListener('offline', this.offLine, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+            alert("pronto");
       
           /*Preso da http://blog.safaribooksonline.com/2012/02/29/phonegap-storing-and-retrieving-with-the-filesystem/ */
                   
@@ -37,7 +38,10 @@ var app = {
                   alert("qua");
                   window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail); 
                   alert("qua2");
-     } 
+     },
+     offLine: function() {
+              window.location = "offline.html";
+          } 
      
 };
  
