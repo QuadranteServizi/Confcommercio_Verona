f57a461c27562faf094f28f9a1b1086b57a2608b
QuadranteServizi <ced@quadranteservizi.it>
2014-03-24 16:06:18 +0100

    087

--- www/js/index.js
+++ www/js/index.js
@@ -4,46 +4,50 @@ function init() {
 }
 
 function checkPreAuth() {
-	console.log("checkPreAuth");
-    var form = $("#loginForm");
     if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined) {
-        $("#username", form).val(window.localStorage["username"]);
-        $("#password", form).val(window.localStorage["password"]);
+        document.getElementById('username').value=window.localStorage["username"];
+        document.getElementById('password').value=window.localStorage["password"];
         handleLogin();
     }
 }
 
-function handleLogin() {
-    var form = $("#loginForm");    
+function handleLogin() {   
     //disable the button so we can't resubmit while we wait
-    $("#submitButton",form).attr("disabled","disabled");
-    var u = $("#username", form).val();
-    var p = $("#password", form).val();
+    document.getElementById("submitButton").disabled = true; 
+    var u = document.getElementById('username').value;
+    var p = document.getElementById('password').value;
+    
     if(u != '' && p!= '') {
-        $.post("http://www.coldfusionjedi.com/demos/2011/nov/10/service.cfc?method=login&returnformat=json", {username:u,password:p}, function(res) {
-            if(res == true) {
-                //store
-                window.localStorage["username"] = u;
-                window.localStorage["password"] = p;             
-                $.mobile.changePage("index.html");
-            } else {
-                navigator.notification.alert("Your login failed", function() {});
-            }
-         $("#submitButton").removeAttr("disabled");
-        },"json");
+       //Cripto username e password per l'invio
+       var key = "cd027Kgh";
+       var crp_usr = stringToHex(des(key, u, 1, 0));
+       var crp_pwd = stringToHex(des(key, p, 1, 0));
+       //Verifico che le credenziali siano giuste
+       var url='http://www.confcommercioverona.it/app/check_login.php?usr='+crp_usr+'&pwd='+crp_pwd;
+       if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+          var http=new XMLHttpRequest();
+       }else {
+          var http=new ActiveXObject("Microsoft.XMLHTTP");
+       }
+                      
+       http.open('HEAD', url, false);
+       http.send();
+      if (http.status==404){
+          alert("Le credenziali di accesso salvate non sono corrette!");
+          document.getElementById("submitButton").disabled = false;
+      }else{
+          window.localStorage["username"] = u;
+          window.localStorage["password"] = p;
+          window.location.replace("index.html"); 
+      }           
     } else {
-        navigator.notification.alert("You must enter a username and password", function() {});
-        $("#submitButton").removeAttr("disabled");
+        navigator.notification.alert("Devi inserire username e password!", function() {});
+        document.getElementById("submitButton").disabled = false;
     }
     return false;
 }
 
 function deviceReady() {
-	console.log("deviceReady");
-	$("#loginPage").on("pageinit",function() {
-		console.log("pageinit run");
-		$("#loginForm").on("submit",handleLogin);
-		checkPreAuth();
-	});
+	$("#loginPage").on("pageinit",function() {	$("#loginForm").on("submit",handleLogin);  		checkPreAuth();  	});
 	$.mobile.changePage("#loginPage");
 }
